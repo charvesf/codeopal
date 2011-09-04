9d29a888885d1396ed4eb761d20905d86af64930
Cyrus Harvesf <charvesf@gmail.com>
2011-09-03 18:54:37 -0700

    Add a basic DirectX program that cycles the window color

---
+++ CodeOpalX/stdafx.h
@@ -0,0 +1,30 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// TODO: reference additional headers your program requires here
+// DirectX
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
